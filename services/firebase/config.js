f0e5eef9f1aca6881976521f1d4dcaf8b22eb34d
mattuycoque <63086689+mattuycoque@users.noreply.github.com>
2022-05-16 17:39:15 +0800

    Update config.js

--- services/firebase/config.js
+++ services/firebase/config.js
@@ -1,9 +1,9 @@
 export const config = {
-  apiKey: 'AIzaSyAjJG6UrfA1FEzV8qbjmuk8wiUic0uWv-8',
-  authDomain: 'finapp-17474.firebaseapp.com',
-  databaseURL: 'https://finapp-17474.firebaseio.com',
-  projectId: 'finapp-17474',
-  storageBucket: 'finapp-17474.appspot.com',
-  messagingSenderId: '1057184379213',
-  appId: '1:1057184379213:web:8c728d0b7099f194',
+  apiKey: 'AIzaSyBQ8hP0Bzz4GQNJd1En7EzFhXD-s5zQBZE',
+  authDomain: 'stoicpanda-a5054.firebaseapp.com',
+  databaseURL: 'https://stoicpanda-a5054-default-rtdb.firebaseio.com',
+  projectId: 'stoicpanda-a5054',
+  storageBucket: 'stoicpanda-a5054.appspot.com',
+  messagingSenderId: '1005404916296',
+  appId: '1:1005404916296:web:740e22ea220173cde5e67d',
 }
